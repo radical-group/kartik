6fffc46ccea2a097d70d2daa608256cf0cd1fa7b
Kartik <kartikrattan09@gmail.com>
2020-04-16 12:15:58 -0400

    emulator benchmarking

---
+++ emulator/benchmarking_metrics/benchmarking_perf/KMeans.py
@@ -0,0 +1,31 @@
+from pandas import DataFrame
+#import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import random
+#from time import time
+#import timeit
+
+x_list=[]
+y_list=[]
+
+for i in range(0,1048576):
+ n1=random.randint(1,100)
+ n2=random.randint(1,100)
+ x_list.append(n1)
+ y_list.append(n2)
+
+#Data = {'x': [25,34,22,27,33,33,31,22,35,34,67,54,57,43,50,57,59,52,65,47,49,48,35,33,44,45,38,43,51,46],
+#       'y': [79,51,53,78,59,74,73,57,69,75,51,32,40,47,53,36,35,58,59,50,25,20,14,12,20,5,29,27,8,7]
+#       }
+Data = {'x':x_list,
+'y':y_list
+}
+df = DataFrame(Data,columns=['x','y'])
+#start = timeit.timeit()
+kmeans = KMeans(n_clusters=3).fit(df)
+centroids = kmeans.cluster_centers_
+#print(centroids)
+
+#plt.scatter(df['x'], df['y'], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
+#plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=50)
+#plt.show()
